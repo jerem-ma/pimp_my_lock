ab7f5312da85dc8c53567ff07f8ff2f856076a88
Jeremy Maia <jmaia@student.42.fr>
2022-03-01 13:19:56 +0100

    Make pimp_my_lock.sh more general

--- pimp_my_lock.sh
+++ pimp_my_lock.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 if [ $# -lt 1 ]; then
-	echo "Usage: $0 <path_to_media> <x> <y> <width> <height>"
+	echo "Usage: $0 <path_to_media> [<x> <y> [<width> <height>]]"
 	exit 1
 fi
 ft_lock
@@ -8,7 +8,7 @@ until grep '"ft_lock":' <(xwininfo -tree -root) > /dev/null; do
 	sleep 1
 done
 sleep 1
-/mnt/nfs/homes/jmaia/conga/goose $1 $2 $3 $4 $5&
+$(dirname $0)/pimp_my_lock $1 $2 $3 $4 $5 &
 pid=$!
 while pgrep -f "ft_lock +-d" > /dev/null; do
 	sleep 1
