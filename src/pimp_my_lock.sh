ac158215ca018de717829e270f1e88b99d2f09f8
Jeremy Maia <jmaia@student.42.fr>
2023-02-25 19:03:03 +0100

    Add _help function

--- src/pimp_my_lock.sh
+++ src/pimp_my_lock.sh
@@ -1,8 +1,40 @@
 #!/bin/bash
 
+PROGRAM_NAME=$0
+
 _main()
 {
 	
+}
+
+_help()
+{
+	echo "Usage: $PROGRAM_NAME <media> [<x> <y>] [<width>] [<height>]"
+	echo ''
+
+	column -L -t -s "	" << EOF
+<media>	:	Path to media. Must be a file readable by mpv
+<x>	:	Position of media in x axis. See POSITION for accepted values
+<y>	:	Position of media in y axis. See POSITION for accepted values
+<width>	:	Width of media. See SIZE for accepted values
+<height>	:	Width of media. See SIZE for accepted values
+EOF
+	echo ''
+
+	cat << EOF
+--- POSITION ---
+Value must be provided either:
+- In pixel
+- In percentage of the screen followed by the sign '%'
+- With one of these keywords:
+	+ For x: left, center, right
+	+ For y: top, center, bottom
+
+--- SIZE ---
+Value must be provided either:
+- In pixel
+- In percentage of the screen followed by the sign '%'
+EOF
 }
 
 _main "$@"; exit
