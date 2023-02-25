ba0cc3eade441c8cb1bfa42157042b9c228f539c
Jeremy Maia <jmaia@student.42.fr>
2023-02-25 19:08:09 +0100

    Create _validate_parameters()

--- src/pimp_my_lock.sh
+++ src/pimp_my_lock.sh
@@ -5,7 +5,105 @@ PROGRAM_NAME=$0
 # Args: <media> [<x> <y>] [<width>] [<height>]
 _main()
 {
-	
+	_validate_parameters "$@"
+}
+
+# Args: Same as main
+_validate_parameters()
+{
+	if [ $# -lt 1  ]; then
+		_help
+		exit 1
+	fi
+	if [ $# -eq 2 ]; then
+		echo "y must be provided if x is provided !" >&2
+		_help
+		exit 1
+	fi
+
+	local media="$1"
+	local x="$2"
+	local y="$3"
+	local width="$4"
+	local height="$5"
+
+	# TODO Test if mpv could read the file
+	if [ ! -f "$media" ]; then
+		echo "$0: $media: No such file or directory" >&2
+		exit 1
+	fi
+	if [ ! -r "$media" ]; then
+		echo "$0: $media: Permission denied" >&2
+		exit 1
+	fi
+
+	if ! _is_valid_x "$x"; then
+		echo "$0: $x: Invalid argument" >&2
+		_help
+		exit 1
+	fi
+
+	if ! _is_valid_y "$y"; then
+		echo "$0: $y: Invalid argument" >&2
+		_help
+		exit 1
+	fi
+
+	if [[ $# -ge 4 ]] && ! _is_valid_size "$width"; then
+		echo "$0: $width: Invalid argument" >&2
+		_help
+		exit 1
+	fi
+
+	if [[ $# -ge 5 ]] && ! _is_valid_size "$height"; then
+		echo "$0: $height: Invalid argument" >&2
+		_help
+		exit 1
+	fi
+}
+
+# Args: <x>
+_is_valid_x()
+{
+	if [ $# -lt 1 ]; then
+		echo "Usage: $0 <x>" >&2
+	fi
+	local x="$1"
+	is_pixel_or_percent "$x" || [[ "$x" = "left" || "$x" = "center" || "$x" = "right" ]]
+	return $?
+}
+
+# Args: <y>
+_is_valid_y()
+{
+	if [ $# -lt 1 ]; then
+		echo "Usage: "$0" <y>" >&2
+	fi
+	local y="$1"
+	is_pixel_or_percent "$y" || [[ "$y" = "top" || "$y" = "center" || "$y" = "bottom" ]]
+	return $?
+}
+
+# Args: <size>
+_is_valid_size()
+{
+	if [ $# -lt 1 ]; then
+		echo "Usage: $0 <size>" >&2
+	fi
+	local size="$1"
+	is_pixel_or_percent "$size"
+	return $?
+}
+
+# Args: <value>
+is_pixel_or_percent()
+{
+	if [ $# -lt 1 ]; then
+		echo "Usage: $0 <value>" >&2
+	fi
+	value="$1"
+	[[ "$value" =~ ^[0-9]+%?$ ]]
+	return $?
 }
 
 _help()
